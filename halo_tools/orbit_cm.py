5649cff37209ad3057bbec4e7a58c3ed0c382493
jngaravitoc <nico.garavito@gmail.com>
2016-09-23 13:04:58 -0700

    center of mass computations

--- halo_tools/orbit_cm.py
+++ halo_tools/orbit_cm.py
@@ -10,6 +10,11 @@ Author: J. Nicolas Garavito-Camargo.
 
 University of Arizona.
 
+To-Do:
+------
+1. Work in the main, writing orbit function.
+2. Verify the Shrinking Sphere algorithm.
+
 """
 
 import numpy as np
@@ -39,7 +44,7 @@ def CM_basic(xyz, vxyz):
       velocity of the CM (vxcm, vycm, vzcm)
 
     """
-    assert (len(xyz[:,0])==len(xyz[:,1]) == len(xyz[:,2]) \
+    assert (len(xyz[:,0])==len(xyz[:,1]) == len(xyz[:,2])== \
             len(vxyz[:,0])==len(vxyz[:,1])==len(vxyz[:,2]))
 
     N = len(xyz[:,0])
@@ -55,7 +60,7 @@ def CM_basic(xyz, vxyz):
     return np.array([xcm, ycm, zcm]), np.array([vxcm, vycm, vzcm])
 
 
-def CM_disk_potential(x, y, z, vx, vy, vz, Pdisk):
+def CM_disk_potential(xyz, vxyz, pot, Inn_radius=2.0):
 
     """
     CM_disk_potential:
@@ -69,79 +74,89 @@ def CM_disk_potential(x, y, z, vx, vy, vz, Pdisk):
     -----------
 
     xyz: numpy.ndarray
-      Disk particle positions
+      Particle positions.
     vxyz: numpy.ndarray
-      Disl particle velocities
-    Pdisk: numpy.ndarray
-      Potential of the disk particles.
+      Particle velocities.
+    pot: numpy.ndarray
+      Particle potential.
+    Inn_radius: float, default 2kpc
+      Inner radius at which the most bound
+      particles are going to be.
 
     Output:
     -------
+    r_cm: numpy.ndarray
+      positions of the CM
+    v_cm: numpy.ndarray
+      velocity of the CM
 
     """
 
-    min_pot = np.where(Pdisk==min(Pdisk))[0]
-    x_min = x[min_pot]
-    y_min = y[min_pot]
-    z_min = z[min_pot]
-    # This >2.0 corresponds to the radius in kpc of the particles that
-    # I am taking into account to compute the CM
-    avg_particles = np.where(np.sqrt((x-x_min)**2.0 + (y-y_min)**2.0 +\
-                            (z-z_min)**2.0)<V_radius)[0]
+    min_pot = np.where(pot==min(pot))[0]
+    x_min = xyz[min_pot,0]
+    y_min = xyz[min_pot,1]
+    z_min = xyz[min_pot,2]
 
-    xyz = np.array([x[avg_particles], y[avg_particles], z[avg_particles]]).T
-    vxyz = np.array([vx[avg_particles], vy[avg_particles], vz[avg_particles]]).T
+    avg_particles = np.where(np.sqrt((xyz[:,0]-x_min)**2.0 + (xyz[:,1]-y_min)**2.0 +\
+                            (xyz[:,2]-z_min)**2.0)<Inn_radius)[0]
 
-    r_cm, v_cm = CM_basic(xyz, vxyz)
+    r_cm, v_cm = CM_basic(xyz[avg_particles], vxyz[avg_particles])
 
     return r_cm, v_cm
 
-# Function that computes the CM iteratively
-def CM(x, y, z, vx, vy, vz, delta=0.025):
-    N = len(x)
-    xCM = sum(x)/N
-    yCM = sum(y)/N
-    zCM = sum(z)/N
 
-    xCM_new = xCM
-    yCM_new = yCM
-    zCM_new = zCM
+def CM_iterative(xyz, vxyz, delta=0.025):
+
+    """
+    Compute the center of mass coordinates and velocities of a halo
+    using the Shrinking Sphere Method Power et al 2003.
+    It iterates in radii until reach a convergence given by delta
+    or 1% of the total number of particles.
+
+    Parameters:
+    -----------
+    xyz: numpy.ndarray (n,3)
+      cartesian coordinates of the halo
+
+    vxys: numpy.ndarray (n,3)
+      cartesian velocities of the halo
+
+    delta(optional): float (defautl=0.025kpc)
+      Precision of the CM,
+
+    Returns:
+    --------
+    rcm: numpy.ndarray (1,3
+      cartesian coordinates of the CM.
+    vcm: numpy.ndarray(1,3)
+      cartesian velocity the CM.
+
+    """
+    rCM_new, vCM_new = CM_basic(xyz, vxyz)
 
     xCM = 0.0
     yCM = 0.0
     zCM = 0.0
 
-    vxCM_new = sum(vx)/N
-    vyCM_new = sum(vy)/N
-    vzCM_new = sum(vz)/N
 
-    R1 = np.sqrt((x - xCM_new)**2 + (y - yCM_new)**2 + (z - zCM_new)**2)
-    i=0
-    while (np.sqrt((xCM_new-xCM)**2 + (yCM_new-yCM)**2 +(zCM_new-zCM)**2) > delta):
-        xCM = xCM_new
-        yCM = yCM_new
-        zCM = zCM_new
-        Rcm = np.sqrt(xCM**2 + yCM**2 + zCM**2)
-        R = np.sqrt((x - xCM)**2 + (y - yCM)**2 + (z - zCM)**2)
+    while (np.sqrt((rCM_new[0]-xCM)**2 + (rCM_new[1]-yCM)**2 + (rCM_new[2]-zCM)**2) > delta):
+
+        xCM = rCM_new[0]
+        yCM = rCM_new[1]
+        zCM = rCM_new[2]
+
+        R = np.sqrt((xyz[:,0] - xCM)**2 + (xyz[:,1] - yCM)**2 + (xyz[:,2] - zCM)**2)
         Rmax = max(R)
-        index = np.where(R<Rmax/1.3)[0]
-        x = x[index]
-        y = y[index]
-        z = z[index]
-        vx = vx[index]
-        vy = vy[index]
-        vz = vz[index]
-        N = len(x)
-        i+=1
-        xCM_new = sum(x)/N
-        yCM_new = sum(y)/N
-        zCM_new = sum(z)/N
-        vxCM_new = sum(vx)/N
-        vyCM_new = sum(vy)/N
-        vzCM_new = sum(vz)/N
-        #Rnow[i] = max(np.sqrt((x - xCM_new[i])**2 + (y - yCM_new[i])**2 + (z - zCM_new[i])**2))
-    #clean = np.where(Rnow != 0)[0]
-    return xCM_new, yCM_new, zCM_new, vxCM_new, vyCM_new, vzCM_new
+
+        # Rate at which the sphere is reducing with the enclosed
+        # particles.reducing 2.5%
+        index = np.where(R<Rmax*0.975)[0]
+        xyz = xyz[index]
+        vxyz = vxyz[index]
+
+        rCM_new, vCM_new = CM_basic(xyz, vxyz)
+
+    return rCM_new, vCM_new
 
 # function that computes the CM using the 10% most bound particles!
 # I am using the potential method any more, its not useful to find the 
@@ -169,87 +184,75 @@ def potential_CM(potential, x, y, z, vx, vy, vz):
 
 #Function that computes the CM of the halo using the minimum of the
 #potential:
-def CMMW(x, y, z, pot):
-    rcut = np.where(np.sqrt(x**2+y**2+z**2)<30.0)[0]
-    x, y, z, pot = x[rcut], y[rcut], z[rcut], pot[rcut]
-    cm = np.where(pot == min(pot))[0]
-    x_cm, y_cm, z_cm = x[cm], y[cm], z[cm]
-    return x_cm, y_cm, z_cm
 
-def CMLMC(x, y, z, pot, xcmmw, ycmmw, zcmmw):
-    xcm = sum(x)/len(x)
-    ycm = sum(y)/len(y)
-    zcm = sum(z)/len(z)
-    rcut = np.where(np.sqrt((x-xcm)**2+(y-ycm)**2+(z-zcm)**2)<20.0)[0]
-    x, y, z, pot = x[rcut], y[rcut], z[rcut], pot[rcut]
-    cm = np.where(pot == min(pot))[0]
-    x_cm, y_cm, z_cm = x[cm], y[cm], z[cm]
-    return x_cm, y_cm, z_cm
+def CM_host_potential(xyz, vxyz, pot, Rinn=2.0):
 
-def VCM(x, y, z, xcm, ycm, zcm, vx, vy, vz):
-    Ntot = len(x)
-    N = Ntot
-    while(N>0.1*Ntot):
-        rshell = np.sqrt((x-xcm)**2 + (y-ycm)**2 + (z-zcm)**2)
-        rcut = max(rshell) / 1.1
-        cut = np.where(rshell<=rcut)[0]
-        x, y, z = x[cut], y[cut], z[cut]
-        vx, vy, vz = vx[cut], vy[cut], vz[cut]
-        N = len(x)
-    vxcm = sum(vx)/N
-    vycm = sum(vy)/N
-    vzcm = sum(vz)/N
-    return vxcm, vycm, vzcm
 """
 
-for i in range(i_n, i_f + 1):
-    print 'snapshot' + str(i)
-    time[i-i_n] = readheader(path + snap + "_{:0>3d}".format(i),'time')
-    positions = readsnap(path + snap + "_{:0>3d}".format(i),'pos', 'dm')
-    velocities = readsnap(path + snap + "_{:0>3d}".format(i), 'vel', 'dm')
-    particles_ids = readsnap(path + snap + "_{:0>3d}".format(i), 'pid', 'dm')
-    potential = readsnap(path + snap + "_{:0>3d}".format(i), 'pot','disk')
-    positions_d = readsnap(path + snap + "_{:0>3d}".format(i),'pos','disk')
-    v_d = readsnap(path + snap + "_{:0>3d}".format(i),'vel','disk')
+def orbit_CM(path, snap_name, initial_snap, final_snap, NMW_particles,\
+             delta=0.025, lmc=False):
 
-    ID = np.sort(particles_ids)
-    # The first set of particles are from the host DM halo, the
-    # second set are from the satellite DM halo, the limit is know by
-    # the number of particles in the host halo.
-    idcut = ID[Nhost-1]
-    #index_mw = np.where(particles_ids<=idcut)
-    index_LMC = np.where(particles_ids>idcut)
+    """
+    Computes the orbit of the host and the satellite.
+    The COM of the host and the satellite are compute using the
+    shrinking sphere method at each snapshot.
 
-    x_mw = positions_d[:,0]
-    y_mw = positions_d[:,1]
-    z_mw = positions_d[:,2]
-    #x_mw = positions[index_mw[0],0]
-    #y_mw = positions[index_mw[0],1]
-    #z_mw = positions[index_mw[0],2]
-    x_lmc = positions[index_LMC[0],0]
-    y_lmc = positions[index_LMC[0],1]
-    z_lmc = positions[index_LMC[0],2]
+    Parameters:
+    -----------
 
-    vx_mw = v_d[:,0]
-    vy_mw = v_d[:,1]
-    vz_mw = v_d[:,2]
-    #vx_mw = velocities[index_mw[0],0]
-    #vy_mw = velocities[index_mw[0],1]
-    #vz_mw = velocities[index_mw[0],2]
-    vx_lmc = velocities[index_LMC[0],0]
-    vy_lmc = velocities[index_LMC[0],1]
-    vz_lmc = velocities[index_LMC[0],2]
+    path: Path to the simulation snapshots
+    snap_name: Base name of the snaphot without the number and
+    file type, e.g: LMCMW
+    initial_snap: Number of the initial snapshot
+    final_snap: Number of the final snapshot
+    NMW_particles: Number of MW particles in the simulation.
+    delta: convergence distance
+    lmc: track the lmc orbit. (default = False)
 
-    #potmw = potential[index_mw]
-    #potlmc = potential[index_LMC]
+    Output:
+    -------
+    XMWcm, vMWcm, xLMCcm, vLMCcm: 4 arrays containing the coordinates
+    and velocities of the center of mass with reference to a (0,0,0) point
+    at a given time.
 
-    X[i-i_n], Y[i-i_n], Z[i-i_n], VX[i-i_n], VY[i-i_n], VZ[i-i_n] = CM_disk_potential(x_mw, y_mw, z_mw, vx_mw, vy_mw, vz_mw, potential)
-    Xsat[i-i_n], Ysat[i-i_n], Zsat[i-i_n], VXsat[i-i_n], VYsat[i-i_n], VZsat[i-i_n]= CM(x_lmc, y_lmc, z_lmc, vx_lmc, vy_lmc, vz_lmc, D)
-    Rgal[i-i_n] = np.sqrt((X[i-i_n] - Xsat[i-i_n])**2 + (Y[i-i_n]-Ysat[i-i_n])**2 +(Z[i-i_n] - Zsat[i-i_n])**2)
-    Vgal[i-i_n] = np.sqrt((VX[i-i_n] - VXsat[i-i_n])**2 + (VY[i-i_n]-VYsat[i-i_n])**2 + (VZ[i-i_n] - VZsat[i-i_n])**2)
+    """
 
+    N_snaps = final_snap - initial_snap + 1
+    MW_rcm = np.zeros((N_snaps,3))
+    MW_vcm = np.zeros((N_snaps,3))
+    LMC_rcm = np.zeros((N_snaps,3))
+    LMC_vcm = np.zeros((N_snaps,3))
+
+    for i in range(initial_snap, final_snap+1):
+        xyz = readsnap(path + snap_name + '_{:03d}.hdf5'.format(i),'pos', 'dm')
+        vxyz = readsnap(path + snap_name +'_{:03d}.hdf5'.format(i),'vel', 'dm')
+        pids = readsnap(path + snap_name +'_{:03d}.hdf5'.format(i),'pid', 'dm')
+        if lmc==True:
+            MW_xyz, MW_vxyz, LMC_xyz, LMC_vxyz = MW_LMC_particles(xyz, vxyz, pids, NMW_particles)
+            MW_rcm[i], MW_vcm[i] = CM(MW_xyz, MW_vxyz, delta)
+            LMC_rcm[i], LMC_vcm[i] = CM(LMC_xyz, LMC_vxyz, delta)
+        else:
+            MW_rcm[i], MW_vcm[i] = CM(xyz, vxyz, delta)
+    return MW_rcm, MW_vcm, LMC_rcm, LMC_vcm
+
+
+def writing_file(out_name, Host_rcm, Host_vcm, Sat_rcm ,Sat_vcm):
+    """
+
+    Function that writes the output.
+
+    """
+    f = open(out_name, 'w')
+    f.write("#Time(Gyrs) | Rgal(kpc) | Xsat[kpc] | Ysat[kpc] | Zsat[kpc] |Xhost[kpc] | Yhost[kpc] Zhost[kpc] |"\
+            "Vgal | Vxsat | Vysat | Vzsat | Vxhost | Vyhost | Vzhost |\n")
+
+    for i in range(0, len(Rgal)):
+        f.write("%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n"%(Rgal[i], Xsat[i], Ysat[i],\
+        Zsat[i], X[i], Y[i], Z[i], Vgal[i], VXsat[i], VYsat[i], VZsat[i], VX[i], VY[i], VZ[i]))
+    f.close()
 
 if __name__ == '__main__':
+
     if len(sys.argv) != 8:
         print('Usage: python orbit_cm.py snap_base_name inital_snap_number'\
               'final_snap_number path2file out?name  #DMhost #DMsat')
@@ -271,38 +274,8 @@ if __name__ == '__main__':
     path = str(sys.argv[4]) #'../../data/LMCMW/MW1LMC4/a1/'
 
     # Number of Snapshots
-    N_snaps = (i_f - i_n) + 1
+    MW_rcm, MW_vcm, LMC_rcm, LMC_vcm = orbit_CM(path, snap, i_n, i_f, Nhost)
 
-    D = 0.025 # precision of the CM computation in Kpc.
-    V_radius = 2.0 # Radius of disk particles to compute the CM
-    #Position and velocity arrays for the host and the satellite
-    X = np.zeros(N_snaps)
-    Y = np.zeros(N_snaps)
-    Z = np.zeros(N_snaps)
+    D = 0.025 # precision of the CM computation in Kpc
 
-    VX = np.zeros(N_snaps)
-    VY = np.zeros(N_snaps)
-    VZ = np.zeros(N_snaps)
-
-    Xsat = np.zeros(N_snaps)
-    Ysat = np.zeros(N_snaps)
-    Zsat = np.zeros(N_snaps)
-
-    VXsat = np.zeros(N_snaps)
-    VYsat = np.zeros(N_snaps)
-    VZsat = np.zeros(N_snaps)
-
-    #Galactocentric distance and time arrays
-    Rgal = np.zeros(N_snaps)
-    Vgal = np.zeros(N_snaps)
-    time = np.zeros(N_snaps)
-
-    print 'Writing the data'
-    f = open(out_name, 'w')
-    f.write("#Time(Gyrs) | Rgal(kpc) | Xsat[kpc] | Ysat[kpc] | Zsat[kpc] |Xhost[kpc] | Yhost[kpc] Zhost[kpc] |"\
-            "Vgal | Vxsat | Vysat | Vzsat | Vxhost | Vyhost | Vzhost |\n")
-
-    for i in range(0, len(Rgal)):
-        f.write("%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n"%(time[i], Rgal[i], Xsat[i], Ysat[i],\
-        Zsat[i], X[i], Y[i], Z[i], Vgal[i], VXsat[i], VYsat[i], VZsat[i], VX[i], VY[i], VZ[i]))
-    f.close()
+    print('Writing the data')
